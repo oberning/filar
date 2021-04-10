314c2d18d60370d4da2a1ecd8aeddd827b553517
Oliver Berning <oliver_berning@yahoo.de>
2021-04-10 22:25:09 +0200

    Correct Cargo.toml
    Format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::{fs, io, path::{self, PathBuf}, usize};
 use regex::Regex;
+use std::{
+    fs, io,
+    path::{self, PathBuf},
+    usize,
+};
 
 const PATH: &str = "C:\\Users\\HP\\Downloads\\TOSEC\\Applications\\[TZX]";
 const CHUNK_SIZE: usize = 50;
@@ -9,7 +13,13 @@ fn main() -> io::Result<()> {
     let filtered = files_regex(&entries, &regex);
     let mut last_path = "";
     for chunk in filtered.chunks(CHUNK_SIZE) {
-        let first_path = chunk.first().unwrap().file_name().unwrap().to_str().unwrap();
+        let first_path = chunk
+            .first()
+            .unwrap()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
         println!("First: {:#?}", first_path);
         println!("Last: {:#?}", last_path);
         last_path = chunk.last().unwrap().file_name().unwrap().to_str().unwrap();
@@ -21,14 +31,15 @@ fn main() -> io::Result<()> {
 fn files(path: &str) -> Result<Vec<path::PathBuf>, io::Error> {
     let mut entries = fs::read_dir(path)?
         .filter(|entry| entry.as_ref().unwrap().path().is_file())
-        .map(|entry| entry.unwrap().path()) 
+        .map(|entry| entry.unwrap().path())
         .collect::<Vec<_>>();
     entries.sort_by(|a, b| a.file_name().unwrap().cmp(b.file_name().unwrap()));
     Ok(entries)
 }
 
 fn files_regex(v: &Vec<PathBuf>, regex: &Regex) -> Vec<PathBuf> {
-    v.to_owned().into_iter()
-    .filter(|entry| regex.is_match(entry.file_name().unwrap().to_str().unwrap()))
-    .collect::<Vec<_>>()
+    v.to_owned()
+        .into_iter()
+        .filter(|entry| regex.is_match(entry.file_name().unwrap().to_str().unwrap()))
+        .collect::<Vec<_>>()
 }
