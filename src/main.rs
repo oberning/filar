2a531f611dd702da99ba9a23c612e0fd907af121
Oliver Berning <oliver_berning@yahoo.de>
2021-04-10 23:45:33 +0200

    Add CTRL+SHIFT+B for fast runs
    Add function for file name comparison

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 const PATH: &str = "C:\\Users\\HP\\Downloads\\TOSEC\\Applications\\[TZX]";
 const CHUNK_SIZE: usize = 50;
+
 fn main() -> io::Result<()> {
     let entries = files(PATH)?;
     let regex = Regex::new("^[^[A-Z]]").unwrap();
@@ -20,8 +21,8 @@ fn main() -> io::Result<()> {
             .unwrap()
             .to_str()
             .unwrap();
-        println!("First: {:#?}", first_path);
-        println!("Last: {:#?}", last_path);
+        let (first, last) = dir_part_name(first_path, last_path);
+        println!("First: {} - Last: {}", first, last);
         last_path = chunk.last().unwrap().file_name().unwrap().to_str().unwrap();
     }
     //println!("Ergebnis: {:#?}, Size: {}", test, test.len());
@@ -42,4 +43,19 @@ fn files_regex(v: &Vec<PathBuf>, regex: &Regex) -> Vec<PathBuf> {
         .into_iter()
         .filter(|entry| regex.is_match(entry.file_name().unwrap().to_str().unwrap()))
         .collect::<Vec<_>>()
+}
+
+fn dir_part_name(s1: &str, s2: &str) -> (String, String) {
+    if s2.len() == 0 {
+        return (s1[0..1].to_string(), "".to_string());
+    }
+    let max_length = if s1.len() > s2.len() { s1.len() } else { s2.len() };
+    for i in 0..max_length {
+        let i1 = if i < s1.len() { i } else { s1.len()-1 };
+        let i2 = if i < s2.len() { i } else { s2.len()-1 };
+        if s1[0..i1] != s2[0..i2] {
+            return (s1[0..i1].to_string(), s2[0..i2].to_string());
+        }
+    }
+    ("".to_string(), "".to_string()) // That should never happen
 }
